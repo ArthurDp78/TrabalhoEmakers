a5bdda0dcfc56e0980ce1f24231cf4a0b709a25f
Arthur <arthursoaresmarques78@gmail.com>
2024-12-25 18:45:39 -0300

    Primeira Parte Login Concluida

--- tela_login/src/App.jsx
+++ tela_login/src/App.jsx
@@ -3,15 +3,19 @@ import './App.css';
 
 function App() {
   return(
-    <div>
+  <div>
+    <div className='Grupo'>
       <h1 className='login'>Login</h1>
       <form className='forms'>
           <input type="email" placeholder='E-mail'></input>
           <input type="senha" placeholder='Senha'></input>
       </form>
-      <button className='confirmar'></button>
-      
     </div>
+
+    <div className='Grupo2'>
+      <button className= 'confirmar'></button>
+    </div>
+  </div>
   );
 }
 
